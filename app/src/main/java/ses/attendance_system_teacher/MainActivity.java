7bd4dcd8662f88f19d5fd67c4096b5d62a425a72
Tien Long Lam <lamtienlong9@gmail.com>
2021-03-05 11:05:03 +0700

    Main Activity

---
+++ app/src/main/java/ses/attendance_system_teacher/MainActivity.java
@@ -0,0 +1,14 @@
+package ses.attendance_system_teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
